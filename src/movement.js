11d5619b9031a9097b52cebae0e81c3b892eb5be
NoNotNow <riviere.manuel@gmail.com>
2025-08-15 14:22:06 +0400

    WIP nextFree

--- src/movement.js
+++ src/movement.js
@@ -6,10 +6,16 @@ import { checkObstacleCollision } from './game-state.js';
 import { updateView } from './view-renderer.js';
 import { beep } from './audio-player.js';
 
-
+export function nextRight(){
+  let startFreeRight=free(1);
+  for(let n=1; n<free();n++){
+    //todo get Position and add one in the direction 
+    //check free to the right return n when > startFreeRight
+  }
+}
 
 // Check how many steps the avatar can move from a pint to a direction without hitting an obstacle
-export function free(inX, inY, directionOffset) {
+export function free(directionOffset, inX, inY) {
   // todo verify input 
   let x = inX;
   let y = inY;
