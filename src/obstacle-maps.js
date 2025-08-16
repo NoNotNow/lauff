5dac24c852958425b6c4bba7643b850c66b3685e
NoNotNow <riviere.manuel@gmail.com>
2025-08-16 17:10:22 +0400

    Updated obstacle-maps.js

--- src/obstacle-maps.js
+++ src/obstacle-maps.js
@@ -245,7 +245,7 @@ export const obstacleMapFalten = [
   {"x":18,"y":19}
 ];
 
-export obstacleMapJosie2 = 
+export obstacleMapJosie2 = [{"x":12,"y":10},{"x":4,"y":17},{"x":7,"y":7},{"x":15,"y":6},{"x":9,"y":14},{"x":8,"y":11},{"x":9,"y":6},{"x":18,"y":14},{"x":14,"y":19},{"x":1,"y":12},{"x":5,"y":3},{"x":20,"y":3},{"x":15,"y":15},{"x":11,"y":18},{"x":8,"y":20},{"x":15,"y":13},{"x":3,"y":8},{"x":13,"y":2},{"x":2,"y":20},{"x":18,"y":9},{"x":8,"y":2},{"x":18,"y":5},{"x":3,"y":4},{"x":20,"y":12},{"x":21,"y":7},{"x":4,"y":12},{"x":6,"y":16},{"x":1,"y":16},{"x":12,"y":6},{"x":0,"y":6}]
 
 export const obstacleMaps = {
   'map1': { name: 'Labyrinth', obstacles: obstacleMap1 },
